2f3b4a7192b81b4de0ef299f5f0239a47a3036f2
yi-dai <851758415@qq.com>
2020-08-03 10:52:18 +0800

    implement test for empty string

--- src/main/java/Mommifier.java
+++ src/main/java/Mommifier.java
@@ -1,5 +1,7 @@
 public class Mommifier {
     String convert(String given_string){
+        //int sum_of_vowels;
+
         return given_string.toString();
     }
 }
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -4,14 +4,24 @@ import org.junit.jupiter.api.Test;
 
 public class MommifierTest {
     @Test
-    void should_return_itself_when_vowels_less_30(){
+    void should_return_itself_when_string_is_null(){
         //given
         String string_with_less_30_vowels = "";
         //when
         Mommifier mommifier = new Mommifier();
         String result = mommifier.convert(string_with_less_30_vowels);
         //then
-        assertEquals(string_with_less_30_vowels,result);
+        assertEquals(string_with_less_30_vowels.toString(),result);
+    }
+    @Test
+    void should_return_itself_when_vowels_less_30(){
+        //given
+        String string_with_less_30_vowels = "abcdf";
+        //when
+        Mommifier mommifier = new Mommifier();
+        String result = mommifier.convert(string_with_less_30_vowels);
+        //then
+        assertEquals(string_with_less_30_vowels.toString(),result);
     }
 
     @Test
@@ -22,7 +32,7 @@ public class MommifierTest {
         Mommifier mommifier = new Mommifier();
         String result = mommifier.convert(string_with_not_continuous_vowels);
         //then
-        assertEquals(result,string_with_not_continuous_vowels.toString());
+        assertEquals(string_with_not_continuous_vowels.toString(),result);
     }
-    
+
 }
