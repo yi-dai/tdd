532ac51940d42e5d784de7e91a7274586785c9c2
yi-dai <851758415@qq.com>
2020-08-03 10:40:03 +0800

    implement test for string less than 30

---
+++ src/main/java/Mommifier.java
@@ -0,0 +1,5 @@
+public class Mommifier {
+    String convert(String given_string){
+        return given_string.toString();
+    }
+}
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,2 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
 public class MommifierTest {
+    @Test
+    void should_return_itself_when_vowels_less_30(){
+        //given
+        String string_with_less_30_vowels = "";
+        //when
+        Mommifier mommifier = new Mommifier();
+        String result = mommifier.convert(string_with_less_30_vowels);
+        //then
+        assertEquals(string_with_less_30_vowels,result);
+    }
+
+    @Test
+    void should_return_itself_when_vowels_is_not_continuous(){
+        //given
+        String string_with_not_continuous_vowels = "aibce";
+        //when
+        Mommifier mommifier = new Mommifier();
+        String result = mommifier.convert(string_with_not_continuous_vowels);
+        //then
+        assertEquals(result,string_with_not_continuous_vowels.toString());
+    }
+    
 }
